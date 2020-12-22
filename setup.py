2476f2b5b164c48dc48e2696d11117fafcb0d9e2
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2020-12-22 11:39:39 -0500

    Bump to 1.0.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'python-chrisclient',
-      version          =   '1.0.8',
+      version          =   '1.0.10',
       description      =   '(Python) client for the ChRIS API',
       long_description =   readme(),
       author           =   'FNNDSC',
