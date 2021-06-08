b0a5dea7a6036051b00c9ce5d5fca9d1cd5264c1
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2021-06-08 14:40:19 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'python-chrisclient',
-      version          =   '2.3.0',
+      version          =   '2.3.2',
       description      =   '(Python) client for the ChRIS API',
       long_description =   readme(),
       author           =   'FNNDSC',
